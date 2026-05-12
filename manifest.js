23a63c6ad26edf39811ff62a322c0ac1c5243d7f
Fadi Bahodi <fadi.bahodi@medportal.ca>
2026-05-12 14:53:53 -0500

    Photos wave 3: 327 files / 143 buildings. Mark synthetic (satellite/OSM) photos with figcaption tag — honest provenance.

--- manifest.js
+++ manifest.js
@@ -1,4 +1,192 @@
 window.PHOTO_MANIFEST = {
+  "arc_erin_mills": [
+    "photos/arc_erin_mills_1.jpg",
+    "photos/arc_erin_mills_2.jpg",
+    "photos/arc_erin_mills_3.jpg"
+  ],
+  "battleford_glen_erin": [
+    "photos/battleford_glen_erin_1.jpg",
+    "photos/battleford_glen_erin_2.jpg",
+    "photos/battleford_glen_erin_3.jpg",
+    "photos/battleford_glen_erin_4.jpg",
+    "photos/battleford_glen_erin_5.jpg",
+    "photos/battleford_glen_erin_6.jpg",
+    "photos/battleford_glen_erin_7.jpg"
+  ],
+  "dialogue": [
+    "photos/dialogue_confederation_2.jpg",
+    "photos/dialogue_confederation_3.jpg",
+    "photos/dialogue_confederation_4.jpg",
+    "photos/dialogue_confederation_5.jpg",
+    "photos/dialogue_confederation_1.jpg"
+  ],
+  "eau_du_soleil": [
+    "photos/eau_du_soleil_1.jpg",
+    "photos/eau_du_soleil_2.jpg",
+    "photos/eau_du_soleil_3.jpg",
+    "photos/eau_du_soleil_4.jpg",
+    "photos/eau_du_soleil_5.jpg"
+  ],
+  "elora": [
+    "photos/elora_1.png",
+    "photos/elora_glen_erin_1.png",
+    "photos/elora_glen_erin_2.png",
+    "photos/elora_glen_erin_3.png",
+    "photos/elora_glen_erin_4.png"
+  ],
+  "forty_second_45": [
+    "photos/forty_second_45_1.jpg",
+    "photos/forty_second_45_2.jpg",
+    "photos/forty_second_45_3.jpg",
+    "photos/forty_second_45_4.jpg"
+  ],
+  "james_90": [
+    "photos/james_90_2.jpg",
+    "photos/james_90_1.jpg"
+  ],
+  "lake_shore_155": [
+    "photos/lake_shore_155_1.jpg",
+    "photos/lake_shore_155_2.jpg",
+    "photos/lake_shore_155_3.jpg",
+    "photos/lake_shore_155_4.jpg",
+    "photos/lake_shore_155_5.jpg"
+  ],
+  "lake_shore_2335": [
+    "photos/lake_shore_2335_2.jpg",
+    "photos/lake_shore_2335_1.jpg"
+  ],
+  "lakeshore_760": [
+    "photos/lakeshore_760_1.jpg",
+    "photos/lakeshore_760_2.jpg",
+    "photos/lakeshore_760_3.jpg"
+  ],
+  "lakewood_1257": [
+    "photos/lakewood_1257_1.jpg",
+    "photos/lakewood_1257_2.jpg",
+    "photos/lakewood_1257_3.jpg",
+    "photos/lakewood_1257_4.jpg",
+    "photos/lakewood_1257_5.jpg"
+  ],
+  "landmark_2493": [
+    "photos/landmark_2493_1.jpg",
+    "photos/landmark_2493_2.jpg"
+  ],
+  "lincoln_545": [
+    "photos/lincoln_545_2.jpg",
+    "photos/lincoln_545_3.jpg",
+    "photos/west_mall_545_1.png",
+    "photos/west_mall_545_2.jpg",
+    "photos/west_mall_545_3.jpg",
+    "photos/lincoln_545_1.jpg"
+  ],
+  "markwood_4365": [
+    "photos/markwood_4365_2.jpg",
+    "photos/markwood_4365_3.jpg",
+    "photos/markwood_4365_1.jpg"
+  ],
+  "mill_road_340": [
+    "photos/mill_road_340_2.jpg",
+    "photos/mill_road_340_3.jpg",
+    "photos/mill_road_340_4.jpg",
+    "photos/mill_road_340_5.jpg",
+    "photos/mill_road_340_1.jpg"
+  ],
+  "mimico_estates": [
+    "photos/mimico_estates_1.jpg",
+    "photos/mimico_estates_2.jpg",
+    "photos/mimico_estates_3.jpg",
+    "photos/mimico_estates_4.jpg",
+    "photos/mimico_estates_5.jpg"
+  ],
+  "newholm_8": [
+    "photos/newholm_8_2.jpg",
+    "photos/newholm_8_1.jpg"
+  ],
+  "park_lawn_285": [
+    "photos/park_lawn_285_2.jpg",
+    "photos/park_lawn_285_1.jpg"
+  ],
+  "promenade_2301": [
+    "photos/promenade_2301_1.jpg",
+    "photos/promenade_2301_2.jpg",
+    "photos/promenade_2301_3.jpg"
+  ],
+  "reid_drive": [
+    "photos/reid_drive_1.jpg",
+    "photos/reid_drive_2.jpg",
+    "photos/reid_drive_3.jpg",
+    "photos/reid_drive_4.jpg",
+    "photos/reid_drive_5.jpg"
+  ],
+  "shoreline_towers": [
+    "photos/shoreline_2313_2.jpg",
+    "photos/shoreline_2313_1.jpg"
+  ],
+  "collegeway_2285": [
+    "photos/the_collegeway_2285_2.jpg",
+    "photos/the_collegeway_2285_3.jpg",
+    "photos/the_collegeway_2285_4.jpg",
+    "photos/the_collegeway_2285_5.jpg",
+    "photos/the_collegeway_2285_1.jpg"
+  ],
+  "walden_circle": [
+    "photos/walden_circle_1.jpg",
+    "photos/walden_circle_2.jpg",
+    "photos/walden_circle_3.jpg",
+    "photos/walden_circle_4.jpg",
+    "photos/walden_circle_5.jpg"
+  ],
+  "dialogue_confederation": [
+    "photos/dialogue_confederation_1.jpg",
+    "photos/dialogue_confederation_2.jpg",
+    "photos/dialogue_confederation_3.jpg",
+    "photos/dialogue_confederation_4.jpg",
+    "photos/dialogue_confederation_5.jpg"
+  ],
+  "elora_glen_erin": [
+    "photos/elora_glen_erin_1.png",
+    "photos/elora_glen_erin_2.png",
+    "photos/elora_glen_erin_3.png",
+    "photos/elora_glen_erin_4.png",
+    "photos/elora_glen_erin_5.png"
+  ],
+  "mimico_estates_official": [
+    "photos/mimico_estates_official_1.jpg"
+  ],
+  "miracle_4900_glen_erin": [
+    "photos/miracle_4900_glen_erin_1.jpg",
+    "photos/miracle_4900_glen_erin_2.jpg",
+    "photos/miracle_4900_glen_erin_3.jpg",
+    "photos/miracle_4900_glen_erin_4.jpg",
+    "photos/miracle_4900_glen_erin_5.jpg",
+    "photos/miracle_4900_glen_erin_6.jpg",
+    "photos/miracle_4900_glen_erin_7.jpg"
+  ],
+  "shoreline_2313": [
+    "photos/shoreline_2313_1.jpg",
+    "photos/shoreline_2313_2.jpg"
+  ],
+  "skyrise_ref": [
+    "photos/skyrise_ref_1.jpg",
+    "photos/skyrise_ref_2.jpg"
+  ],
+  "station_place_5249": [
+    "photos/station_place_5249_1.jpg"
+  ],
+  "the_collegeway_2285": [
+    "photos/the_collegeway_2285_1.jpg",
+    "photos/the_collegeway_2285_2.jpg",
+    "photos/the_collegeway_2285_3.jpg",
+    "photos/the_collegeway_2285_4.jpg",
+    "photos/the_collegeway_2285_5.jpg"
+  ],
+  "west_mall_545": [
+    "photos/west_mall_545_1.png",
+    "photos/west_mall_545_2.jpg",
+    "photos/west_mall_545_3.jpg",
+    "photos/west_mall_545_4.jpg",
+    "photos/west_mall_545_5.jpg"
+  ],
   "1011_lorne_park": [
     "photos/1011_lorne_park_1.jpg",
     "photos/1011_lorne_park_2.png"
@@ -19,11 +207,6 @@ window.PHOTO_MANIFEST = {
     "photos/applewood_towers_1055_1.jpg",
     "photos/applewood_towers_1055_2.png"
   ],
-  "arc_erin_mills": [
-    "photos/arc_erin_mills_1.jpg",
-    "photos/arc_erin_mills_2.jpg",
-    "photos/arc_erin_mills_3.jpg"
-  ],
   "argyle_2570": [
     "photos/argyle_2570_1.jpg",
     "photos/argyle_2570_2.png"
@@ -40,16 +223,21 @@ window.PHOTO_MANIFEST = {
     "photos/battleford_2797_1.jpg",
     "photos/battleford_2797_2.png"
   ],
-  "battleford_glen_erin": [
-    "photos/battleford_glen_erin_1.jpg",
-    "photos/battleford_glen_erin_2.jpg",
-    "photos/battleford_glen_erin_3.jpg",
-    "photos/battleford_glen_erin_4.jpg",
-    "photos/battleford_glen_erin_5.jpg"
+  "birmingham_45": [
+    "photos/birmingham_45_1.jpg",
+    "photos/birmingham_45_2.png"
   ],
   "bloor_1475": [
     "photos/bloor_1475_1.jpg",
     "photos/bloor_1475_2.png"
+  ],
+  "bloor_3939": [
+    "photos/bloor_3939_1.jpg",
+    "photos/bloor_3939_2.png"
+  ],
+  "bloor_4001": [
+    "photos/bloor_4001_1.jpg",
+    "photos/bloor_4001_2.png"
   ],
   "bridgewood_1867": [
     "photos/bridgewood_1867_1.jpg",
@@ -95,13 +283,6 @@ window.PHOTO_MANIFEST = {
     "photos/daniels_gateway_5625_1.jpg",
     "photos/daniels_gateway_5625_2.png"
   ],
-  "dialogue": [
-    "photos/dialogue_confederation_1.jpg",
-    "photos/dialogue_confederation_2.jpg",
-    "photos/dialogue_confederation_3.jpg",
-    "photos/dialogue_confederation_4.jpg",
-    "photos/dialogue_confederation_5.jpg"
-  ],
   "dixie_square_1750": [
     "photos/dixie_square_1750_1.jpg",
     "photos/dixie_square_1750_2.png"
@@ -110,12 +291,9 @@ window.PHOTO_MANIFEST = {
     "photos/dundas_120e_1.jpg",
     "photos/dundas_120e_2.png"
   ],
-  "eau_du_soleil": [
-    "photos/eau_du_soleil_1.jpg",
-    "photos/eau_du_soleil_2.jpg",
-    "photos/eau_du_soleil_3.jpg",
-    "photos/eau_du_soleil_4.jpg",
-    "photos/eau_du_soleil_5.jpg"
+  "eighth_24": [
+    "photos/eighth_24_1.jpg",
+    "photos/eighth_24_2.png"
   ],
   "elizabeth_20n": [
     "photos/elizabeth_20n_1.jpg",
@@ -133,13 +311,6 @@ window.PHOTO_MANIFEST = {
     "photos/elmwoods_30_1.jpg",
     "photos/elmwoods_30_2.png"
   ],
-  "elora": [
-    "photos/elora_1.png",
-    "photos/elora_glen_erin_1.png",
-    "photos/elora_glen_erin_2.png",
-    "photos/elora_glen_erin_3.png",
-    "photos/elora_glen_erin_4.png"
-  ],
   "embassy_99_agnes": [
     "photos/embassy_99_agnes_1.jpg",
     "photos/embassy_99_agnes_2.png"
@@ -151,12 +322,6 @@ window.PHOTO_MANIFEST = {
   "forestwoods_1547": [
     "photos/forestwoods_1547_1.jpg",
     "photos/forestwoods_1547_2.png"
-  ],
-  "forty_second_45": [
-    "photos/forty_second_45_1.jpg",
-    "photos/forty_second_45_2.jpg",
-    "photos/forty_second_45_3.jpg",
-    "photos/forty_second_45_4.jpg"
   ],
   "havenwood_3410": [
     "photos/havenwood_3410_1.jpg",
@@ -178,10 +343,6 @@ window.PHOTO_MANIFEST = {
     "photos/inverhouse_920_1.jpg",
     "photos/inverhouse_920_2.png"
   ],
-  "james_90": [
-    "photos/james_90_1.jpg",
-    "photos/james_90_2.jpg"
-  ],
   "kaneff_3575": [
     "photos/kaneff_3575_1.jpg",
     "photos/kaneff_3575_2.png"
@@ -190,20 +351,25 @@ window.PHOTO_MANIFEST = {
     "photos/kaneff_3620_1.jpg",
     "photos/kaneff_3620_2.png"
   ],
+  "kingsway_old_mill_2": [
+    "photos/kingsway_old_mill_2_1.jpg",
+    "photos/kingsway_old_mill_2_2.png"
+  ],
+  "kipling_50_s": [
+    "photos/kipling_50_s_1.jpg",
+    "photos/kipling_50_s_2.png"
+  ],
   "lago_waterfront": [
     "photos/lago_waterfront_1.jpg",
     "photos/lago_waterfront_2.png"
   ],
-  "lake_shore_155": [
-    "photos/lake_shore_155_1.jpg",
-    "photos/lake_shore_155_2.jpg",
-    "photos/lake_shore_155_3.jpg",
-    "photos/lake_shore_155_4.jpg",
-    "photos/lake_shore_155_5.jpg"
+  "lake_promenade_220": [
+    "photos/lake_promenade_220_1.jpg",
+    "photos/lake_promenade_220_2.png"
   ],
-  "lake_shore_2335": [
-    "photos/lake_shore_2335_1.jpg",
-    "photos/lake_shore_2335_2.jpg"
+  "lake_promenade_240": [
+    "photos/lake_promenade_240_1.jpg",
+    "photos/lake_promenade_240_2.png"
   ],
   "lakeshore_206e": [
     "photos/lakeshore_206e_1.jpg",
@@ -213,10 +379,29 @@ window.PHOTO_MANIFEST = {
     "photos/lakeshore_212e_1.jpg",
     "photos/lakeshore_212e_2.png"
   ],
-  "lakeshore_760": [
-    "photos/lakeshore_760_1.jpg",
-    "photos/lakeshore_760_2.jpg",
-    "photos/lakeshore_760_3.jpg"
+  "lakeshore_2212": [
+    "photos/lakeshore_2212_1.jpg",
+    "photos/lakeshore_2212_2.png"
+  ],
+  "lakeshore_2230": [
+    "photos/lakeshore_2230_1.jpg",
+    "photos/lakeshore_2230_2.png"
+  ],
+  "lakeshore_3479": [
+    "photos/lakeshore_3479_1.jpg",
+    "photos/lakeshore_3479_2.png"
+  ],
+  "lakeshore_3590": [
+    "photos/lakeshore_3590_1.jpg",
+    "photos/lakeshore_3590_2.png"
+  ],
+  "lakeshore_3650": [
+    "photos/lakeshore_3650_1.jpg",
+    "photos/lakeshore_3650_2.png"
+  ],
+  "lakeshore_3845": [
+    "photos/lakeshore_3845_1.jpg",
+    "photos/lakeshore_3845_2.png"
   ],
   "lakeside_place_2699": [
     "photos/lakeside_place_2699_1.jpg",
@@ -226,27 +411,17 @@ window.PHOTO_MANIFEST = {
     "photos/lakeview_place_2770_1.jpg",
     "photos/lakeview_place_2770_2.png"
   ],
-  "lakewood_1257": [
-    "photos/lakewood_1257_1.jpg",
-    "photos/lakewood_1257_2.jpg",
-    "photos/lakewood_1257_3.jpg",
-    "photos/lakewood_1257_4.jpg",
-    "photos/lakewood_1257_5.jpg"
-  ],
   "lakewood_ii_1285": [
     "photos/lakewood_ii_1285_1.jpg",
     "photos/lakewood_ii_1285_2.png"
   ],
-  "landmark_2493": [
-    "photos/landmark_2493_1.jpg",
-    "photos/landmark_2493_2.jpg"
+  "legion_165": [
+    "photos/legion_165_1.jpg",
+    "photos/legion_165_2.png"
   ],
-  "lincoln_545": [
-    "photos/lincoln_545_1.jpg",
-    "photos/lincoln_545_2.jpg",
-    "photos/lincoln_545_3.jpg",
-    "photos/west_mall_545_1.png",
-    "photos/west_mall_545_2.jpg"
+  "legion_185": [
+    "photos/legion_185_1.jpg",
+    "photos/legion_185_2.png"
   ],
   "linwood_1785": [
     "photos/linwood_1785_1.jpg",
@@ -256,47 +431,37 @@ window.PHOTO_MANIFEST = {
     "photos/lolita_620_1.jpg",
     "photos/lolita_620_2.png"
   ],
+  "long_branch_30": [
+    "photos/long_branch_30_1.jpg",
+    "photos/long_branch_30_2.png"
+  ],
   "maplewoods_1477": [
     "photos/maplewoods_1477_1.jpg",
     "photos/maplewoods_1477_2.png"
   ],
-  "markwood_4365": [
-    "photos/markwood_4365_1.jpg",
-    "photos/markwood_4365_2.jpg",
-    "photos/markwood_4365_3.jpg"
+  "marine_parade_15": [
+    "photos/marine_parade_15_1.jpg",
+    "photos/marine_parade_15_2.png"
+  ],
+  "marine_parade_3": [
+    "photos/marine_parade_3_1.jpg",
+    "photos/marine_parade_3_2.png"
+  ],
+  "marine_parade_58": [
+    "photos/marine_parade_58_1.jpg",
+    "photos/marine_parade_58_2.png"
+  ],
+  "markland_dr_3": [
+    "photos/markland_dr_3_1.jpg",
+    "photos/markland_dr_3_2.png"
   ],
   "meadowvale_gardens_2869": [
     "photos/meadowvale_gardens_2869_1.jpg",
     "photos/meadowvale_gardens_2869_2.png"
   ],
-  "mill_road_340": [
-    "photos/mill_road_340_1.jpg",
-    "photos/mill_road_340_2.jpg",
-    "photos/mill_road_340_3.jpg",
-    "photos/mill_road_340_4.jpg",
-    "photos/mill_road_340_5.jpg"
-  ],
-  "mimico_estates": [
-    "photos/mimico_estates_1.jpg",
-    "photos/mimico_estates_2.jpg",
-    "photos/mimico_estates_3.jpg",
-    "photos/mimico_estates_4.jpg",
-    "photos/mimico_estates_5.jpg"
-  ],
   "miracle_4900": [
     "photos/miracle_4900_1.jpg",
     "photos/miracle_4900_2.png"
-  ],
-  "miracle_4900_glen_erin": [
-    "photos/miracle_4900_glen_erin_1.jpg",
-    "photos/miracle_4900_glen_erin_2.jpg",
-    "photos/miracle_4900_glen_erin_3.jpg",
-    "photos/miracle_4900_glen_erin_4.jpg",
-    "photos/miracle_4900_glen_erin_5.jpg"
-  ],
-  "newholm_8": [
-    "photos/newholm_8_1.jpg",
-    "photos/newholm_8_2.jpg"
   ],
   "nsr_275": [
     "photos/nsr_275_1.jpg",
@@ -322,6 +487,10 @@ window.PHOTO_MANIFEST = {
     "photos/paisley_95_1.jpg",
     "photos/paisley_95_2.png"
   ],
+  "palace_pier_1": [
+    "photos/palace_pier_1_1.jpg",
+    "photos/palace_pier_1_2.png"
+  ],
   "park_12e": [
     "photos/park_12e_1.jpg",
     "photos/park_12e_2.png"
@@ -342,18 +511,17 @@ window.PHOTO_MANIFEST = {
     "photos/park_70e_1.jpg",
     "photos/park_70e_2.png"
   ],
-  "park_lawn_285": [
-    "photos/park_lawn_285_1.jpg",
-    "photos/park_lawn_285_2.jpg"
+  "park_lawn_15": [
+    "photos/park_lawn_15_1.jpg",
+    "photos/park_lawn_15_2.png"
+  ],
+  "park_lawn_36": [
+    "photos/park_lawn_36_1.jpg",
+    "photos/park_lawn_36_2.png"
   ],
   "park_royal_2360": [
     "photos/park_royal_2360_1.jpg",
     "photos/park_royal_2360_2.png"
-  ],
-  "promenade_2301": [
-    "photos/promenade_2301_1.jpg",
-    "photos/promenade_2301_2.jpg",
-    "photos/promenade_2301_3.jpg"
   ],
   "promenade_court": [
     "photos/promenade_court_1.jpg",
@@ -363,12 +531,13 @@ window.PHOTO_MANIFEST = {
     "photos/queen_south_190_1.jpg",
     "photos/queen_south_190_2.png"
   ],
-  "reid_drive": [
-    "photos/reid_drive_1.jpg",
-    "photos/reid_drive_2.jpg",
-    "photos/reid_drive_3.jpg",
-    "photos/reid_drive_4.jpg",
-    "photos/reid_drive_5.jpg"
+  "queensway_1430": [
+    "photos/queensway_1430_1.jpg",
+    "photos/queensway_1430_2.png"
+  ],
+  "queensway_955": [
+    "photos/queensway_955_1.jpg",
+    "photos/queensway_955_2.png"
   ],
   "rivergate_35": [
     "photos/rivergate_35_1.jpg",
@@ -411,25 +580,12 @@ window.PHOTO_MANIFEST = {
     "photos/sherobee_2170_2.png"
   ],
   "shore_breeze_30": [
-    "photos/shore_breeze_30_1.jpg"
-  ],
-  "shoreline_towers": [
-    "photos/shoreline_2313_1.jpg",
-    "photos/shoreline_2313_2.jpg"
-  ],
-  "station_place_5249": [
-    "photos/station_place_5249_1.jpg"
+    "photos/shore_breeze_30_1.jpg",
+    "photos/shore_breeze_30_2.png"
   ],
   "strathroy_1470": [
     "photos/strathroy_1470_1.jpg",
     "photos/strathroy_1470_2.png"
-  ],
-  "collegeway_2285": [
-    "photos/the_collegeway_2285_1.jpg",
-    "photos/the_collegeway_2285_2.jpg",
-    "photos/the_collegeway_2285_3.jpg",
-    "photos/the_collegeway_2285_4.jpg",
-    "photos/the_collegeway_2285_5.jpg"
   ],
   "the_huron_2475": [
     "photos/the_huron_2475_1.jpg",
@@ -447,16 +603,29 @@ window.PHOTO_MANIFEST = {
     "photos/vita_on_the_lake_1.jpg",
     "photos/vita_on_the_lake_2.png"
   ],
-  "walden_circle": [
-    "photos/walden_circle_1.jpg",
-    "photos/walden_circle_2.jpg",
-    "photos/walden_circle_3.jpg",
-    "photos/walden_circle_4.jpg",
-    "photos/walden_circle_5.jpg"
-  ],
   "waterford_2645": [
     "photos/waterford_2645_1.jpg",
     "photos/waterford_2645_2.png"
+  ],
+  "west_mall_320": [
+    "photos/west_mall_320_1.jpg",
+    "photos/west_mall_320_2.png"
+  ],
+  "west_mall_350": [
+    "photos/west_mall_350_1.jpg",
+    "photos/west_mall_350_2.png"
+  ],
+  "west_mall_390": [
+    "photos/west_mall_390_1.jpg",
+    "photos/west_mall_390_2.png"
+  ],
+  "west_mall_400": [
+    "photos/west_mall_400_1.jpg",
+    "photos/west_mall_400_2.png"
+  ],
+  "western_battery_15": [
+    "photos/western_battery_15_1.jpg",
+    "photos/western_battery_15_2.png"
   ],
   "westwood_1465": [
     "photos/westwood_1465_1.jpg",
